b33ffd5867dc8c8a52cdf3c764e9fb26ac1818cd
catherineambrad <ambradcatherine81@gmail.com>
2023-10-08 02:23:07 +0100

    add relation in product with category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,6 +12,12 @@ class Product extends Model
         'name',
         'description',
         'price',
+        'category_id',
         'image'
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
