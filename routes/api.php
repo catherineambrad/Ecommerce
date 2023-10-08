c74be885d932a2d24e20d61e406080bd92617d3a
catherineambrad <ambradcatherine81@gmail.com>
2023-10-08 02:25:37 +0100

    create api for categories

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,13 +23,18 @@ Route::get('/login', [AuthController::class, 'login']);
 // Public routes
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
-Route::get('/products/search/{name}', [ProductController::class, 'search']);
+Route::post('/products/search', [ProductController::class, 'search']);
+Route::get('/categories', [CategoryController::class, 'index']);
 
 // Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/products', [ProductController::class, 'store']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+    Route::post('/categories', [CategoryController::class,'store']);
+    Route::put('/categories/{id}', [CategoryController::class, 'edit']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
     Route::post('/logout', [AuthController::class, 'logout']);
 });
