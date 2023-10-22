8e261fa73659f4efebfad21572a341736ab3ac4d
catherineambrad <ambradcatherine81@gmail.com>
2023-10-22 18:33:16 +0100

    optimize action login

--- app/Actions/UserActions/LoginUserAction.php
+++ app/Actions/UserActions/LoginUserAction.php
@@ -3,6 +3,7 @@
 namespace App\Actions\UserActions;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class LoginUserAction
@@ -10,14 +11,18 @@ class LoginUserAction
     public function execute(array $data)
     {
         // Check email 
-        $user = User::where('email', $data['email'])->first();
+        $user = User::whereEmail($data['email'])->first();
 
         // Check password
-        if (!$user || !Hash::check($data['password'], $user->password))
-            return response(['message' => 'Bad cred'], 401);
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response(['message' => 'Bad credentials'], 401);
+        }
 
-        $token = $user->createToken('myapptoken')->plainTextToken;
+        // Attempt to log in the user after password validation
+        $token = $user->createToken('api')->plainTextToken;
 
-        return response(['user' => $user, 'token' => $token], 201)->withCookie('token', $token, config('jwt.ttl'), '/', 'localhost', false, false);
+        if (Auth::attempt($data)) {
+            return response()->json(['user' => $user, 'token' => $token], 201)->withCookie('token', $token, config('jwt.ttl'), '/', 'localhost', false, false);
+        }
     }
 }
