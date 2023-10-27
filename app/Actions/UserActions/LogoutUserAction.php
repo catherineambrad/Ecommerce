db9275ad8d450a1abc64db85c61564b9f892f3ac
catherineambrad <ambradcatherine81@gmail.com>
2023-10-27 23:20:33 +0100

    optimize remove token from cookie

--- app/Actions/UserActions/LogoutUserAction.php
+++ app/Actions/UserActions/LogoutUserAction.php
@@ -2,17 +2,15 @@
 
 namespace App\Actions\UserActions;
 
-use Illuminate\Support\Facades\Cookie;
-
 class LogoutUserAction
 {
     public function execute()
     {
         auth()->user()->token()->revoke();
-        Cookie::forget('token');
+
         return  response()->json([
             'success' => true,
             'message' => 'Logged out successfully'
-        ], 200);
+        ], 200)->withoutCookie('token');
     }
 }
